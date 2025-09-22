a4694302ab6ad3fe0f0b193cdd1e2dfc17f3029f
Anand S <anand1946s@gmail.com>
2025-09-22 18:05:44 +0530

    Delete ui directory

--- ui/ChatWindow.java
+++
@@ -1,8 +0,0 @@
-package ui;
-
-public class ChatWindow {
-    
-}
-
-//(optional)  opens a window t chat with seller
-// alternetley we can provide only the contact no and skip this page if no time
--- ui/PostProduct.java
+++
@@ -1,7 +0,0 @@
-package ui;
-
-public class PostProduct {
-    
-}
-
-// opens a form to fill details about the product.and a post button.
